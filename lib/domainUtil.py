34308e9d535d8343a655100cb4e32ac93bc3f5f1
黎宇豪 <muyoutoudemutou@163.com>
2022-10-09 21:27:00 +0800

    修改为扫描完成删除

--- lib/domainUtil.py
+++ lib/domainUtil.py
@@ -1,23 +1,25 @@
 # coding=utf-8
 # !/usr/bin/env python3
+import sys
 
 from lib.sub import Process
 from lib.utils import getTime
 import csv,time
 
 def getChildDomain(mysql):
-    while True:
-        try:
+    try:
+        while True:
+
             #获取目标
-            result = mysql.execute('select id,domain from target limit 0,1;')
+            result = mysql.execute('select id,domain from target limit 0,1;')[0]
             #更新扫描时间
-            mysql.execute('delete from target where id=%s',(result[0]['id']))
+            mysql.execute('delete from target where id=%s',(result['id']))
 
             #调用工具开始爆破子域名
-            print('[+] 开始 %s 子域名扫描,命令如下:'%result[0]['domain'])
-            Process('python3 tools/OneForAll/oneforall.py --target %s --path domains.csv --alive true --fmt csv run'%result[0]['domain']).exe(outFlag=False)
+            print('[+] 开始 %s 子域名扫描,命令如下:'%result['domain'])
+            Process('python3 tools/OneForAll/oneforall.py --target %s --path domains.csv --alive true --fmt csv run'%result['domain']).exe(outFlag=False)
 
-            print('[+] 根域 %s 扫描结束，发现子域名：' % result[0]['domain'])
+            print('[+] 根域 %s 扫描结束，发现子域名：' % result['domain'])
             rows = []
             with open('domains.csv','r') as domains:
                 domain_csv = csv.DictReader(domains)
@@ -28,5 +30,5 @@ def getChildDomain(mysql):
             if len(rows) > 0:
                 mysql.execute('replace into domains(url,subdomain,status,ip) value(%s,%s,%s,%s);',args=rows)
             Process('rm -rf tools/OneForAll/result/*').exe(outFlag=False)
-        except KeyboardInterrupt:
-            mysql.execute('insert into target(domain) value(%s);'% result['domain'])
+    except KeyboardInterrupt:
+        mysql.execute('insert into target(domain) value(%s);'% result['domain'])
