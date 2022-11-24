131abe231f9881ee38add985b9ae6ba45d5c129f
黎宇豪 <muyoutoudemutou@163.com>
2022-11-24 21:06:48 +0800

    改成批量扫500条

--- lib/scan.py
+++ lib/scan.py
@@ -20,7 +20,7 @@ class scanProcess():
             exepopen = Popen('./xray ws --basic-crawler %s --html-output ../../xrayresult/%s.html' % (
             self.url,getTime('%Y%m%d%H%M%S')), stdout=PIPE, shell=True, env=env)
         elif self.type == 'nuclei':
-            exepopen = Popen('../nuclei -rl 1000 -bs 100 -c 100  -mhe 10 -ni -u %s -o ../../xrayresult/%s-nuclei.txt -stats -silent -severity critical,medium,high,low' % (
+            exepopen = Popen('../nuclei -l ../../nucleitarget.txt -rl 300 -bs 35 -c 30 -mhe 10 -ni -u %s -o ../../xrayresult/%s-nuclei.txt -stats -silent -severity critical,medium,high,low' % (
                 self.url, getTime('%Y%m%d%H%M%S')), stdout=PIPE, shell=True, env=env)
         try:
             while True:
@@ -46,12 +46,17 @@ def scan(mysql):
                 result = mysql.execute('select id,url,remark from domains where remark = "scan" limit 0,1;')[0]
             except IndexError:
                 try:
-                    result = mysql.execute('select id,url,remark from domains where remark = "bountyscan" limit 0,1;')[0]
+                    result = mysql.execute('select id,url,remark from domains where remark = "bountyscan" limit 0,500;')
+                    ids = []
+                    with open('nucleitarget.txt', 'w')as nfile:
+                        for row in result:
+                            nfile.write(row['url']+'\n')
+                            ids.append(row['id'])
                 except IndexError:
                     print('无子域名，等待5分钟。')
                     time.sleep(300)
 
-            mysql.execute('delete from domains where id=%s;', (result['id']))
+            mysql.execute('delete from domains where id=%s;',args=ids)
             if result['remark'] == 'scan':
                 scanProcess(result['url'], 'xray').exe()
             elif result['remark'] == 'bountyscan':
@@ -60,4 +65,4 @@ def scan(mysql):
     except KeyboardInterrupt:
         pass
     finally:
-        mysql.execute('insert into domains(id,url,remark) value(%s,%s,%s);', (result['id'],result['url'],result['remark']))
+        mysql.execute('insert into domains(id,url,remark) value(%s,%s,%s);', args=result)
